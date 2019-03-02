959307c02c48d38ed209e5bc7b3da3dc22b8da81
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2019-03-02 14:15:43 +0400

    utils: add signature serialization helper.

--- test/util-test.js
+++ test/util-test.js
@@ -5,6 +5,10 @@
 
 const assert = require('./util/assert');
 const util = require('../lib/utils/transaction');
+const common = require('../lib/protocol/common');
+const Signature = require('../lib/utils/signature');
+
+const sigVector = require('./data/signature.json');
 
 const responseTests = [{
   messages: [
@@ -61,5 +65,94 @@ describe('utils', function () {
         assert.bufferEqual(messages[i], test.messages[i]);
       }
     }
+  });
+});
+
+function checkSignature(vector, lsig) {
+  assert.bufferEqual(lsig.r, vector.r, 'R is not correct.');
+  assert.bufferEqual(lsig.s, vector.s, 'S is not correct.');
+  assert.strictEqual(lsig.recid, vector.param, 'RecId is not correct.');
+  assert.bufferEqual(lsig.encode(), vector.sigrs, 'RS encoding error.');
+  assert.bufferEqual(lsig.toDER(), vector.dersig, 'DER encoding error.');
+  assert.bufferEqual(lsig.toLedgerSignature(), vector.ledgersig,
+    'Ledger signature encoding error.');
+  assert.bufferEqual(lsig.toCoreSignature(), vector.core,
+    'Bitcoin Core signature encoding error.');
+
+  const compressedPublicKey = lsig.recover(vector.encodedMessage, true);
+  const uncompressedPublicKey = lsig.recover(vector.encodedMessage, false);
+
+  assert.bufferEqual(compressedPublicKey, vector.compressedPublicKey,
+    'Recovered compressed public key does not match.');
+  assert.bufferEqual(uncompressedPublicKey, vector.uncompressedPublicKey,
+    'Recovered uncompressed public key does not match.');
+
+  assert.ok(lsig.verify(vector.encodedMessage, vector.compressedPublicKey),
+    'Verification failed for compressed public key.'
+  );
+
+  assert.ok(lsig.verify(vector.encodedMessage, vector.uncompressedPublicKey),
+    'Verification failed for uncompressed public key.'
+  );
+}
+
+describe('Signature', function () {
+  const vector = {};
+
+  before(() => {
+    for (const [k, v] of Object.entries(sigVector)) {
+      if (k === 'param' || k === 'originalMessage') {
+        vector[k] = v;
+        continue;
+      }
+
+      vector[k] = Buffer.from(v, 'hex');
+    }
+  });
+
+  it('should encode message', () => {
+    const encoded = common.encodeMessage(vector.originalMessage);
+
+    assert.bufferEqual(encoded, vector.encodedMessage);
+  });
+
+  it('should create sig from Options', () => {
+    const sig = Signature.fromOptions({
+      r: vector.r,
+      s: vector.s,
+      recid: vector.param
+    });
+
+    checkSignature(vector, sig);
+  });
+
+  it('should create sig from RS and recid', () => {
+    const sig = Signature.decode(vector.sigrs);
+
+    // decode from RS wont recover recid
+    sig.setRecid(vector.param);
+
+    checkSignature(vector, sig);
+  });
+
+  it('should create sig from DER and recid', () => {
+    const sig = Signature.fromDER(vector.dersig);
+
+    // DER does not contain recid either.
+    sig.setRecid(vector.param);
+
+    checkSignature(vector, sig);
+  });
+
+  it('should create sig from Ledger signature format', () => {
+    const sig = Signature.fromLedgerSignature(vector.ledgersig);
+
+    checkSignature(vector, sig);
+  });
+
+  it('should create sig from Core Signature format', () => {
+    const sig = Signature.fromCoreSignature(vector.core);
+
+    checkSignature(vector, sig);
   });
 });
